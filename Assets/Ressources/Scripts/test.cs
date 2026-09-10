3e978540c406400fb50ae3697b760d4ece2d3e76
Melina38 <melinabascol@gmail.com>
2026-09-10 15:08:21 +0200

    init commit player move

---
+++ Assets/Ressources/Scripts/test.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    public test()
+    {
+        
+        
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.test();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
