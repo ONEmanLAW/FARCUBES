ebc1f2eb5730058cb482f81fcff75a7e0f8d9c44
Melina38 <melinabascol@gmail.com>
2026-09-10 16:44:13 +0200

    add player script

--- Assets/Ressources/Scripts/InputControllerPlayer.cs
+++ Assets/Ressources/Scripts/InputControllerPlayer.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class InputControllerPlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //public float speed = 1;
+    public Player player;
 
     // Update is called once per frame
     void Update()
     {
+        //float moveX = 0f;
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            player.Move(1f);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.Move(-1f);
+        }
+
         
     }
 }
