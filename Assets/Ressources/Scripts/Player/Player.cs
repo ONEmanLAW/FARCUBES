83a9acebe44ff6167614fe04d2873019d886fab5
Hugo Culot <hugoculot.dev@gmail.com>
2026-09-11 08:48:13 +0200

    Added Instance of player in Player.

--- Assets/Ressources/Scripts/Player/Player.cs
+++ Assets/Ressources/Scripts/Player/Player.cs
@@ -2,12 +2,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+
     [SerializeField] private float speed = 5f;
     [SerializeField] private Rigidbody body;
 
     private bool isDead;
 
     public bool IsDead => isDead;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Debug.LogWarning($"{name} : un Player existe deja, celui-ci est detruit.");
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+            Instance = null;
+    }
 
     private void Reset()
     {
