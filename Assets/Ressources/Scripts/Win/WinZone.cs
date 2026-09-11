93f18ae52d8a6ae5aaa3883c45b9910ebe5583f4
Hugo Culot <hugoculot.dev@gmail.com>
2026-09-11 13:49:32 +0200

    Made a win text when wining

--- Assets/Ressources/Scripts/Win/WinZone.cs
+++ Assets/Ressources/Scripts/Win/WinZone.cs
@@ -4,9 +4,6 @@ public class WinZone : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        // Debug temporaire : a retirer une fois que la zone fonctionne.
-        Debug.Log($"zone de victoire touchee par {other.name}");
-
         // GetComponentInParent et pas TryGetComponent : le collider peut
         // etre sur un enfant du joueur (le modele par exemple).
         Player player = other.GetComponentInParent<Player>();
